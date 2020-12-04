88ea528150c541832dbdff1397de2ae888817b54
roruther <Thereisafountain7!>
2020-12-03 23:30:21 -0500

    Fix: Modify app.py code.

--- app/app.py
+++ app/app.py
@@ -35,11 +35,11 @@ def index():
     return render_template('index.html', title='Home', user=user, cities=cities_data)
 
 
-@app.route('/api/cities')
-def cities() -> str:
-    js = json.dumps(cities_import())
-    resp = Response(js, status=200, mimetype='application/json')
-    return resp
+#@app.route('/api/cities')
+#def cities() -> str:
+    #js = json.dumps(cities_import())
+    #resp = Response(js, status=200, mimetype='application/json')
+    #return resp
 
 
 if __name__ == '__main__':
